cd8d664ce93831869d026abe3bcd78a86d0ad6ee
popobo <331361996@qq.com>
2024-08-14 23:22:06 +0800

    feat: add prisma and test

---
+++ app/api/test/route.ts
@@ -0,0 +1,15 @@
+import { NextResponse } from "next/server";
+import { prisma } from "@/lib/prisma";
+
+export async function GET() {
+  try {
+    const users = await prisma.user.findMany();
+    return NextResponse.json(users);
+  } catch (error) {
+    console.error("Error fetching users:", error);
+    return NextResponse.json(
+      { error: "Internal Server Error" },
+      { status: 500 }
+    );
+  }
+}
