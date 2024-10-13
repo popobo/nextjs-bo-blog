8a2de16a445556fb44ee1e52b40d4cbd6701cbe7
popobo <331361996@qq.com>
2024-10-13 18:18:10 +0800

    feat: clean project

--- app/about/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function About() {
-  return (
-    <div>
-      <h1 className="text-3xl font-bold mb-4">About Me</h1>
-      <p>Here you can write about yourself and your blog.</p>
-    </div>
-  );
-}
--- app/blog/[slug]/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function BlogPost({ params }: { params: { slug: string } }) {
-  return (
-    <div>
-      <h1 className="text-3xl font-bold mb-4">Blog Post: {params.slug}</h1>
-      <p>This is where the content of the blog post will go.</p>
-    </div>
-  );
-}
